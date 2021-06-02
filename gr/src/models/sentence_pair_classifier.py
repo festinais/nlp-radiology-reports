cd7abe4cb9d74bd72cce08f5bc54593035ed8ece
Festa <festina.ismali@tum.de>
2021-06-03 00:53:11 +0200

    added total_steps

--- gr/src/models/sentence_pair_classifier.py
+++ gr/src/models/sentence_pair_classifier.py
@@ -54,10 +54,8 @@ class SentencePairClassifier(pl.LightningModule):
         self.train_accuracy = pl.metrics.Accuracy()
         self.val_accuracy = pl.metrics.Accuracy()
 
-    def forward(self, input_ids, attn_masks, token_type_ids, labels):
-        return self.model(input_ids, attn_masks, token_type_ids, labels)
-        # features = self.classifier(self.dropout(pooler_output))
-        # return features
+    def forward(self, **inputs):
+        return self.model(**inputs)
 
     def training_step(self, batch, batch_idx):
         outputs = self(**batch)
