dc7db31fde4d2ae6da869e2f07a283f46fe4e51c
Festa <festina.ismali@tum.de>
2021-07-20 23:44:17 +0200

    added mrpc data for testing

---
+++ exp.py
@@ -0,0 +1,18 @@
+from datasets import load_dataset
+import csv
+
+dataset = load_dataset('glue', 'mrpc', split='train')
+my_iter = iter(dataset)
+
+with open('mrpc_data.csv', 'w+') as output:
+    writer = csv.writer(output)
+    writer.writerow(['section_one', 'section_two', 'label'])
+    count = 0
+    for x in my_iter:
+        count += 1
+        print(x['label'])
+        writer.writerow([x['sentence1'], x['sentence2'], x['label']])
+
+
+
+
