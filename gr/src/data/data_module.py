f19507fb3f312fd1cfee1225e123d89336f5e36e
Festa <festina.ismali@tum.de>
2021-08-08 20:04:03 +0200

    data module change

--- gr/src/data/data_module.py
+++ gr/src/data/data_module.py
@@ -7,58 +7,16 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 class CustomDataset(Dataset):
 
-    def __init__(self, data, maxlen=128, with_labels=True, bert_model='albert-base-v2'):
-
+    def __init__(self, data):
         self.data = data  # pandas dataframe
         # Initialize the tokenizer
-        self.tokenizer = AutoTokenizer.from_pretrained(bert_model)
-
-        self.maxlen = maxlen
-        self.with_labels = with_labels
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
-
         # Selecting sentence1 and sentence2 at the specified index in the data frame
-        sent1 = str(self.data.loc[index, 'section_one'])
-        sent2 = str(self.data.loc[index, 'section_two'])
-
-        # Tokenize the pair of sentences to get token ids, attention masks and token type ids
-        encoded_pair = self.tokenizer(sent1, sent2,
-                                      padding='max_length',  # Pad to max_length
-                                      truncation=True,  # Truncate to max_length
-                                      max_length=self.maxlen,
-                                      return_tensors='pt')  # Return torch.Tensor objects
-
-        token_ids = encoded_pair['input_ids'].squeeze(0)  # tensor of token ids
-        attn_masks = encoded_pair['attention_mask'].squeeze(
-            0)  # binary tensor with "0" for padded values and "1" for the other values
-        token_type_ids = encoded_pair['token_type_ids'].squeeze(
-            0)  # binary tensor with "0" for the 1st sentence tokens & "1" for the 2nd sentence tokens
-
-        if self.with_labels:  # True if the dataset has labels
-            label = self.data.loc[index, 'label']
-            return token_ids, attn_masks, token_type_ids, label
-        else:
-            return token_ids, attn_masks, token_type_ids
-
-# class CustomDataset(Dataset):
-#
-#     def __init__(self, data):
-#
-#         self.data = data  # pandas dataframe
-#         # Initialize the tokenizer
-#
-#     def __len__(self):
-#         return len(self.data)
-#
-#     def __getitem__(self, index):
-#
-#         # Selecting sentence1 and sentence2 at the specified index in the data frame
-#         sent1 = str(self.data.loc[index, 'section_one'])
-#         sent2 = str(self.data.loc[index, 'section_two'])
-#
-#         label = self.data.loc[index, 'label']
-#         return sent1, sent2, label
+        sent1 = str(self.data[['section_one']].iloc[[index]])
+        sent2 = str(self.data[['section_two']].iloc[[index]])
+        label = self.data[['label']].iloc[[index]]
+        return sent1, sent2, label
