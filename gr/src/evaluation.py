4e2aeae2a62927c4a5f2c1737ecbd216ef9eb36d
Festa <festina.ismali@tum.de>
2021-08-08 20:28:05 +0200

    test data

--- gr/src/evaluation.py
+++ gr/src/evaluation.py
@@ -25,7 +25,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 
 def get_data():
-    dataset = load_dataset('csv', data_files='gr/data/data.csv')
+    dataset = load_dataset('csv', data_files='gr/data/data_test.csv')
     split = dataset['train'].train_test_split(test_size=0.2, seed=1)  # split the original training data for validation
     train = split['train']
     test = split['test']
@@ -258,5 +258,5 @@ def evaluate_main():
 
 
 if __name__ == "__main__":
-    main()
-    # evaluate_main()
+    # main()
+    evaluate_main()
