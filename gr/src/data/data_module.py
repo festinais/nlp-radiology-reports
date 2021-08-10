137f9c50504bda5e8f30efc65c8e148a196df880
Festa <festina.ismali@tum.de>
2021-08-10 16:15:38 +0200

    evaluate without clean_text

--- gr/src/data/data_module.py
+++ gr/src/data/data_module.py
@@ -44,11 +44,11 @@ class CustomDataset(Dataset):
 
     def __getitem__(self, index):
         # Selecting sentence1 and sentence2 at the specified index in the data frame
-        # sent1 = clean_text(str(self.data[['section_one']].iloc[[index]]))
-        # sent2 = clean_text(str(self.data[['section_two']].iloc[[index]]))
+        sent1 = clean_text(str(self.data[['section_one']].iloc[[index]]))
+        sent2 = clean_text(str(self.data[['section_two']].iloc[[index]]))
 
-        sent1 = str(self.data[['section_one']].iloc[[index]])
-        sent2 = str(self.data[['section_two']].iloc[[index]])
+        # sent1 = str(self.data[['section_one']].iloc[[index]])
+        # sent2 = str(self.data[['section_two']].iloc[[index]])
 
         label = self.data[['label']].iloc[[index]]
         return sent1, sent2, label
