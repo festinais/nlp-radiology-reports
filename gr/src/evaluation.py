10fd243289b3a443c0bfa8f7ba430a5f93d46b34
Festa <festina.ismali@tum.de>
2021-08-19 15:21:29 +0200

    test with 3 samples

--- gr/src/evaluation.py
+++ gr/src/evaluation.py
@@ -37,6 +37,7 @@ def get_data():
     df_val = pd.DataFrame(val)
     df_test = pd.DataFrame(test)
     df_test = pd.read_csv("gr/data/data_no_dup_test.csv")
+    df_test = df_test.head(3)
     print(df_test)
 
     print('{0} {1} length'.format(df_train.shape, 'train'))
