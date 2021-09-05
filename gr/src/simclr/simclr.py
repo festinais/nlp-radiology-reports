ccf8328ea7b62c05e637f897e895d55688dec8f6
Festa <festina.ismali@tum.de>
2021-09-05 02:04:53 +0200

    added mean pooling

--- gr/src/simclr/simclr.py
+++ gr/src/simclr/simclr.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 from simclr.modules.identity import Identity
+import torch
 
 
 class SimCLR(nn.Module):
@@ -26,7 +27,14 @@ class SimCLR(nn.Module):
     def forward(self, input_ids_i, attn_masks_i, token_type_ids_i, input_ids_j, attn_masks_j, token_type_ids_j):
         h_i = self.encoder(input_ids_i, attn_masks_i, token_type_ids_i)
         h_j = self.encoder(input_ids_j, attn_masks_j, token_type_ids_j)
-
-        z_i = self.projector(h_i)
-        z_j = self.projector(h_j)
+        z_i = self.mean_pooling(self.encoder, attn_masks_i)
+        z_j = self.mean_pooling(self.encoder, attn_masks_j)
         return h_i, h_j, z_i, z_j
+
+    # Mean Pooling - Take attention mask into account for correct averaging
+    def mean_pooling(self, model_output, attention_mask):
+        token_embeddings = model_output[0]  # First element of model_output contains all token embeddings
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
+        sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+        return sum_embeddings / sum_mask
