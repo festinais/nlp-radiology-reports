5a1e452e62592e896be677ff7b78a35a6bcc83cd
Festa <festina.ismali@tum.de>
2021-09-06 01:02:55 +0200

    minor fix

--- gr/src/simclr/simclr.py
+++ gr/src/simclr/simclr.py
@@ -35,7 +35,7 @@ class SimCLR(nn.Module):
     # Mean Pooling - Take attention mask into account for correct averaging
     def mean_pooling(self, model_output, attention_mask):
         token_embeddings = model_output  # First element of model_output contains all token embeddings
-        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(model_output.bert_layer.config.embedding_size).float()
         sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
         sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
         return sum_embeddings / sum_mask
