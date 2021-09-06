aafbe1873f4a77d969114049771b8b97931f6249
Festa <festina.ismali@tum.de>
2021-09-06 04:13:25 +0200

    pooler output

--- gr/src/simclr/modules/nt_xent.py
+++ gr/src/simclr/modules/nt_xent.py
@@ -41,6 +41,7 @@ class NT_Xent(nn.Module):
 
         # We have 2N samples, but with Distributed training every GPU gets N examples too, resulting in: 2xNxN
         print("N value", N)
+        print("sim value", torch.cat((sim_i_j, sim_j_i), dim=0).shape)
         positive_samples = torch.cat((sim_i_j, sim_j_i), dim=0).reshape(N, 1)
         negative_samples = sim[self.mask].reshape(N, -1)
 
