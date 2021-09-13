08d013e35cf578f57e94ca8e89d73a552e1ace1e
Festa <festina.ismali@tum.de>
2021-09-13 13:32:43 +0200

    validation fix

--- gr/src/train.py
+++ gr/src/train.py
@@ -15,7 +15,7 @@ def evaluate_loss(net, device, criterion, dataloader, tokenizer):
 
     mean_loss = 0
     count = 0
-
+    acc = 0
     with torch.no_grad():
         for it, (section_ones, section_two, labels) in enumerate(tqdm(dataloader)):
             encoded_pairs_1 = tokenizer(list(section_ones),
@@ -55,14 +55,16 @@ def evaluate_loss(net, device, criterion, dataloader, tokenizer):
                                      token_type_ids_2)
 
             # Computing loss
-            mean_loss += criterion(z_i, z_j)
+            loss, acc = criterion(z_i, z_j)
+
+            mean_loss += loss.item()
             # seq, attn_masks, token_type_ids, labels = \
             #     seq.to(device), attn_masks.to(device), token_type_ids.to(device), labels.to(device)
             # logits = net(seq, attn_masks, token_type_ids)
             # mean_loss += criterion(logits.squeeze(-1), labels.float()).item()
             count += 1
 
-    return mean_loss / count
+    return mean_loss / count, acc
 
 
 def train_bert(net,
@@ -159,12 +161,12 @@ def train_bert(net,
 
                 running_loss = 0.0
 
-        val_loss = evaluate_loss(net, device, criterion, val_loader, tokenizer)  # Compute validation loss
+        val_loss, acc = evaluate_loss(net, device, criterion, val_loader, tokenizer)  # Compute validation loss
         print()
         print("Epoch {} complete! Validation Loss : {}".format(ep + 1, val_loss))
 
         if val_loss < best_loss:
-            print("Best validation loss improved from {} to {}".format(best_loss, val_loss))
+            print("Best validation loss improved from {} to {}. Acc: {}".format(best_loss, val_loss, acc))
             print()
             net_copy = copy.deepcopy(net)  # save a copy of the model
             best_loss = val_loss
