014e3560f30675a0152c8c064055512714d94748
Festa <festina.ismali@tum.de>
2021-09-13 11:43:27 +0200

    cpu device

--- gr/src/evaluation.py
+++ gr/src/evaluation.py
@@ -216,6 +216,8 @@ def main():
         print("Let's use", torch.cuda.device_count(), "GPUs!")
         net = nn.DataParallel(net)
 
+    device = torch.device("cpu")
+
     net.to(device)
 
     # get the data
