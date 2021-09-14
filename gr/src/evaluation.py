f9d25098bcc2ed3457fea379ad15cfb22573795d
Festa <festina.ismali@tum.de>
2021-09-14 22:05:40 +0200

    delete print

--- gr/src/evaluation.py
+++ gr/src/evaluation.py
@@ -184,9 +184,6 @@ def test_prediction(net, device, dataloader, criterion, with_labels=True, result
                                  token_type_ids_2)
         # Computing loss
         loss, acc, logits, labels = criterion(z_i, z_j)
-        print("adding a batch")
-        print("logits: ", logits)
-        print("labels: ", labels)
         metric.add_batch(predictions=logits, references=labels)
 
     final_score = metric.compute()
