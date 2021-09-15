b0030b35e1c611d5c6d96fe3f7a7696755661ad1
Festa <festina.ismali@tum.de>
2021-09-15 10:54:29 +0200

    eval

--- gr/src/evaluation.py
+++ gr/src/evaluation.py
@@ -31,6 +31,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 def get_data():
     dataset = load_dataset('csv', data_files='gr/data/data_no_dup.csv')
+    dataset = load_dataset('csv', data_files='gr/data/mrpc_data.csv')
     split = dataset['train'].train_test_split(test_size=0.2, seed=1)  # split the original training data for validation
     train = split['train']
     test = split['test']
@@ -41,7 +42,7 @@ def get_data():
     df_train = pd.DataFrame(train)
     df_val = pd.DataFrame(val)
     df_test = pd.DataFrame(test)
-    df_test = pd.read_csv("gr/data/data_no_dup_test.csv")
+    # df_test = pd.read_csv("gr/data/data_no_dup_test.csv")
 
     print('{0} {1} length'.format(df_train.shape, 'train'))
     print('{0} {1} length'.format(df_val.shape, 'validation'))
@@ -290,5 +291,5 @@ def evaluate_main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
     evaluate_main()
