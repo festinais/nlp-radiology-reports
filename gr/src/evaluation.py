4c72776e10421ebbc18631273b74ed9c7a17bdc6
Festa <festina.ismali@tum.de>
2021-09-24 10:58:54 +0200

    model load

--- gr/src/evaluation.py
+++ gr/src/evaluation.py
@@ -275,7 +275,7 @@ def evaluate_main():
 
     print()
     print("Loading the weights of the model...")
-    # model.load_state_dict(torch.load(path_to_model))
+    model.load_state_dict(torch.load(path_to_model))
     # model.load_state_dict(torch.load(path_to_model, map_location=device))
     model.to(device)
 
