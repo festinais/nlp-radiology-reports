e97f638e71229b4e4898751fae24420b09009816
Festa <festina.ismali@tum.de>
2021-10-17 17:01:31 +0200

    loss

--- gr/src/train.py
+++ gr/src/train.py
@@ -153,6 +153,7 @@ def train_bert(net,
 
             if (it + 1) % print_every == 0:  # Print training loss information
                 print("print_every: ", print_every)
+                print("loss: ", loss)
                 print()
                 print("Iteration {}/{} of epoch {} complete. Loss : {}. Accuracy: {}"
                       .format(it + 1, nb_iterations, ep + 1, running_loss / print_every, acc))
