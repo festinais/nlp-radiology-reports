49af39f99937918c985270e80e4db09dae0d1882
Festa <festina.ismali@tum.de>
2021-10-19 00:15:54 +0200

    batch

--- gr/src/evaluation.py
+++ gr/src/evaluation.py
@@ -25,23 +25,23 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 
 def get_data():
-    # train = pd.read_csv('gr/data/data_train.csv')
-    # val = pd.read_csv('gr/data/data_val.csv')
-    # test = pd.read_csv('gr/data/data_test.csv')
+    train = pd.read_csv('gr/data/data_train.csv')
+    val = pd.read_csv('gr/data/data_val.csv')
+    test = pd.read_csv('gr/data/data_test.csv')
 
     # split = dataset['train'].train_test_split(test_size=0.2, seed=1)  # split the original training data for validation
     # train = split['train']
     # val = split['test']
-    df = load_dataset('csv', data_files='gr/data_1/mrpc_data.csv')
-
-    split = df['train'].train_test_split(test_size=0.2, seed=1)  # split the original training data for validation
-    train = split['train']
-
-    split_new = train.train_test_split(test_size=0.1, seed=1)
-    train = split_new['train']
-    val = split_new['test']
-
-    test = split['test']
+    # df = load_dataset('csv', data_files='gr/data_1/mrpc_data.csv')
+    #
+    # split = df['train'].train_test_split(test_size=0.2, seed=1)  # split the original training data for validation
+    # train = split['train']
+    #
+    # split_new = train.train_test_split(test_size=0.1, seed=1)
+    # train = split_new['train']
+    # val = split_new['test']
+    #
+    # test = split['test']
 
     df_train = pd.DataFrame(train)
     df_val = pd.DataFrame(val)
@@ -351,5 +351,5 @@ def evaluate_main():
 
 
 if __name__ == "__main__":
-    # main()
+    main()
     evaluate_main()
