9849f4c06355b6abadcbd001ba53fe0971e31af7
Festa <festina.ismali@tum.de>
2021-10-19 23:06:22 +0200

    test

--- gr/src/evaluation.py
+++ gr/src/evaluation.py
@@ -42,7 +42,6 @@ def get_data():
 def collate_fn(batch):
     tokenizer = AutoTokenizer.from_pretrained(get_yaml_parameter("bert_model"))
     maxlen = get_yaml_parameter("maxlen")
-
     token_ids, attn_masks, token_type_ids, labels = [], [], [], []
 
     for index, tuple in enumerate(batch):
@@ -56,8 +55,7 @@ def collate_fn(batch):
                                  truncation=True,  # Truncate to max_length
                                  max_length=maxlen,
                                  return_tensors='pt')  # Return torch.Tensor objects
-        print("sent1: ", sent1)
-        print("sent2: ", sent2)
+
         token_ids.append(encoded_pair['input_ids'].squeeze(0))  # tensor of token ids
         attn_masks.append(encoded_pair['attention_mask'].squeeze(
             0))  # binary tensor with "0" for padded values and "1" for the other values
@@ -82,9 +80,6 @@ def collate_fn(batch):
         token_ids.append(encoded_pair['input_ids'].squeeze(0))  # tensor of token ids
         attn_masks.append(encoded_pair['attention_mask'].squeeze(0))
         token_type_ids.append(encoded_pair['token_type_ids'].squeeze(0))
-        print("-------")
-        print("sent1: ", sent1)
-        print("sent3: ", sent3)
 
         # negative sampling
         if index == len(batch) - 1:
@@ -103,7 +98,6 @@ def collate_fn(batch):
         token_ids.append(encoded_pair_new['input_ids'].squeeze(0))  # tensor of token ids
         attn_masks.append(encoded_pair_new['attention_mask'].squeeze(0))
         token_type_ids.append(encoded_pair_new['token_type_ids'].squeeze(0))
-
 
     return torch.stack(token_ids), torch.stack(attn_masks), torch.stack(token_type_ids), torch.LongTensor(labels)
 
@@ -275,5 +269,5 @@ def evaluate_main():
 
 
 if __name__ == "__main__":
-    # main()
+    main()
     evaluate_main()
